b3a0704721a6a1f165b431e4cf3855570fc580a6
Enginedeer <andi.r.s@live.dk>
2023-02-10 17:38:00 +0100

    Solved task e adequately, with several plots

--- ComputationalPhysics/Homework/Andi_2.jl
+++ ComputationalPhysics/Homework/Andi_2.jl
@@ -125,25 +125,140 @@ RT_Cal_Plot(n,D)
 
 #Subtask e
 n1=1
-n2=2+5*im
-n=transpose([1 n2 n1 n2 n1 n2 n1 n2 n1 n2 1])
+n2=2
+n=transpose([1 n2 n1 n2 n1 2+5*im n1 n2 n1 n2 1])
 D=transpose([1 1 1 1 1 1 1 1 1])
 RT_Cal_Plot(n,D)
 
 ##
 
 n1=1
-n2=2+10*im
-n=transpose([1 n2 n1 n2 n1 n2 n1 n2 n1 n2 1])
+n2=2
+n=transpose([1 n2 n1 n2 n1 2+10*im n1 n2 n1 n2 1])
 D=transpose([1 1 1 1 1 1 1 1 1])
 RT_Cal_Plot(n,D)
 
 ##
 
 n1=1
-n2=2+15*im
-n=transpose([1 n2 n1 n2 n1 n2 n1 n2 n1 n2 1])
+n2=2
+n=transpose([1 n2 n1 n2 n1 2+15*im n1 n2 n1 n2 1])
 D=transpose([1 1 1 1 1 1 1 1 1])
 RT_Cal_Plot(n,D)
+
+
+
+
+
+
+
+
+
+
+## subtask e, n=2+5*im and n=2+10*im
+
+# PLotting T and loss on a logarithmic scale to possibly reveal numerical errors.
+function RT_Cal_Plot3(n,D) #Calculates: Transfermatrix - T and R and loss. For N points. Plots this.
+    N=1001
+    j=1
+    TransM=Array{Float64}(undef,N,1)
+    ReflM=Array{Float64}(undef,N,1)
+    lossM=Array{Float64}(undef,N,1)
+    k₀M=LinRange(0, 3, N)
+    for k₀ in k₀M
+        T=transferM(n,D,k₀)
+        # display(T)
+
+        TransM[j]=abs(T[1,1]-T[1,2]*T[2,1]/T[2,2])^2
+        ReflM[j]=abs(T[2,1]/T[2,2])^2
+        lossM[j]=1-TransM[j]-ReflM[j]
+        j+=1
+    end
+
+
+    Plots.plot(k₀M,lossM, labels="loss", yaxis=:log)
+    Plots.plot!(title = "Relative energy loss of stack of dielectric layers")
+    Plots.plot!(legend=:right)
+    Plots.xlabel!("k₀ [m⁻¹]")
+    loss_plot=Plots.ylabel!("Relative energy loss [ ]")
+    display(loss_plot)
+
+    Plots.plot(k₀M,TransM, labels="T", yaxis=:log)
+    # Plots.plot!(k₀M,ReflM, labels="R")
+    Plots.plot!(title = "Reflectance and Transmittance of stack of dielectric layers")
+    Plots.plot!(legend=:right)
+    Plots.xlabel!("k₀ [m⁻¹]")
+    TR_plot=Plots.ylabel!("Part of energy [ ]")
+    display(TR_plot)
+end
+
+
+n1=1
+n2=2
+n=transpose([1 n2 n1 n2 n1 2+5*im n1 n2 n1 n2 1])
+D=transpose([1 1 1 1 1 1 1 1 1])
+RT_Cal_Plot3(n,D)
+
+##
+
+n1=1
+n2=2
+n=transpose([1 n2 n1 n2 n1 2+10*im n1 n2 n1 n2 1])
+D=transpose([1 1 1 1 1 1 1 1 1])
+RT_Cal_Plot3(n,D)
+# HEY. I think this plot goes to show, that the uncertainty of T 
+# and losses might become very large for large k.
+# Numerical precision seem to give errors for k>1.5, 
+# whereas the result for especially T and loss is less trustworthy.
+# All-inall: There is numerical errors in the results.
+
+
+
+
+
+
+## task e, n=2+15*im
+
+# Different method to calculate R. Exclude T, due to error from numerial precision
+function RT_Cal_Plot2(n,D) #Calculates: Transfermatrix - T and R and loss. For N points. Plots this.
+    N=1001
+    j=1
+    TransM=Array{Float64}(undef,N,1)
+    ReflM=Array{Float64}(undef,N,1)
+    lossM=Array{Float64}(undef,N,1)
+    k₀M=LinRange(0, 3, N)
+    for k₀ in k₀M
+        T=transferM(n,D,k₀)
+        # display(T)
+
+        TransM[j]=abs(T[1,1]-T[1,2]*T[2,1]/T[2,2])^2
+        ReflM[j]=abs(T[2,1]/T[2,2])^2
+        lossM[j]=1-TransM[j]-ReflM[j]
+        j+=1
+    end
+
+
+    # Plots.plot(k₀M,lossM, labels="loss")
+    # Plots.plot!(title = "Relative energy loss of stack of dielectric layers")
+    # Plots.plot!(legend=:right)
+    # Plots.xlabel!("k₀ [m⁻¹]")
+    # loss_plot=Plots.ylabel!("Relative energy loss [ ]")
+    # display(loss_plot)
+
+    # Plots.plot(k₀M,TransM, labels="T")
+    Plots.plot(k₀M,ReflM, labels="R")
+    Plots.plot!(title = "Reflectance and Transmittance of stack of dielectric layers")
+    Plots.plot!(legend=:right)
+    Plots.xlabel!("k₀ [m⁻¹]")
+    TR_plot=Plots.ylabel!("Part of energy [ ]")
+    display(TR_plot)
+end
+
+n1=1
+n2=2
+n=transpose([1 n2 n1 n2 n1 2+15*im n1 n2 n1 n2 1])
+D=transpose([1 1 1 1 1 1 1 1 1])
+RT_Cal_Plot2(n,D)
+
 
 
