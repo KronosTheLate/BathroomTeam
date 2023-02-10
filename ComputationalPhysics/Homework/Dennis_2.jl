4e02b855862a75374acf4f07e7b6883ddc26bc2c
KronosTheLate <densbal@live.no>
2023-02-10 16:17:23 +0100

    Fixing stuff

--- ComputationalPhysics/Homework/Dennis_2.jl
+++ ComputationalPhysics/Homework/Dennis_2.jl
@@ -8,9 +8,6 @@ Return propagation transformation matrix
 """
 P(n, D, k₀) = [cis(-n*D*k₀) 0; 0 cis(-n*D*k₀)]
 
-#Andi's def:
-P(n, D, k₀) = [cis(n*D*k₀) 0; 0 -cis(n*D*k₀)]
-
 """
 Return interface transformation matrix
 """
@@ -19,55 +16,30 @@ I(n₁, n₂) = 1/2 * [
     1-n₂/n₁     1+n₂/n₁
 ]
 
-"""
-Given a vector of refractive indexes n⃗ and 
-distances D⃗, return transfer matrix.
-"""
-function blafunc(n⃗, D⃗, k₀)
+function transfer_matrix(n⃗, D⃗, k₀)
     # Assume first and last transformations are transmissions, with propagation between each.
     # This means that you propegate D⃗[1] in medium of n = n⃗[2], as no propagation occurs in n⃗[1]
 
     # Need 2 values of n for initial and final medium, and both n & D for mediums in between.
     (length(n⃗) == length(D⃗)+2)  ||  throw(ArgumentError("Expected `x+2` refractive indices for `x` distances.\nGot $(length(n⃗)) refractive indices and $(length(D⃗)) distances."))
-    transfer_matrix = I(n⃗[1], n⃗[2])
+    tm = I(n⃗[1], n⃗[2])
     for i in eachindex(D⃗)
-        transfer_matrix = P(n⃗[i+1], D⃗[i], k₀) * transfer_matrix  # propagation
-        transfer_matrix = I(n⃗[i+1], n⃗[i+2])   * transfer_matrix    # interface
+        tm = P(n⃗[i+1], D⃗[i], k₀) * tm  # propagation
+        tm = I(n⃗[i+1], n⃗[i+2])   * tm    # interface
     end
-    println("Displaying transfer_matrix")
-    display(transfer_matrix)
-    @show typeof(transfer_matrix)
-    transfer_matrix
-end
-blafunc([1, 2, 3], [4], 5)
-
-
-function T()
-    my_matrix = rand(2, 2)
-    println("Displaying my_matrix")
-    display(my_matrix)
-    @show typeof(my_matrix)
-    return my_matrix
-end
-𝚃()
-𝚃([1, 2, 3], [4], 5)
-P(1, 1, 1)
-"""
-Transmittance. Typed by \\ttT<tab>
-"""
-function 𝚃(args...)
-    T_ = T(args...)
-    # println("T_ = ")  # printing for troubleshooting...
-    # display(T_)       # values are complex and changing
-    return abs2(T_[1, 1] - T_[1, 2]*T_[2, 1]/T_[2, 2])
+    tm
 end
 
-"""
-Reflectance. Typed by \\ttR<tab>
-"""
-function 𝚁(args...)
-    T_ = T(args...)
-    return abs2(T_[2, 1]/T_[2, 2])
+function transmittance(args...)
+    tm_ = transfer_matrix(args...)
+    # println("tm_ = ")  # printing for troubleshooting...
+    # display(tm_)       # values are complex and changing
+    return abs2(tm_[1, 1] - tm_[1, 2]*tm_[2, 1]/tm_[2, 2])
+end
+
+function reflectance(args...)
+    tm_ = transfer_matrix(args...)
+    return abs2(tm_[2, 1]/tm_[2, 2])
 end
 
 ##
@@ -79,20 +51,19 @@ let # using let block for namespace hygiene
     D⃗s = ones(n_propagations)
     k₀s = range(0, 3, 1000)
 
-    𝚃s = [𝚃(n⃗s, D⃗s, k₀) for k₀ in k₀s]
-    𝚁s = [𝚁(n⃗s, D⃗s, k₀) for k₀ in k₀s]
-    E_lost = 1 .- 𝚃s .- 𝚁s
+    transmittances = [transmittance(n⃗s, D⃗s, k₀) for k₀ in k₀s]
+    reflectances = [reflectance(n⃗s, D⃗s, k₀) for k₀ in k₀s]
+    E_lost = 1 .- transmittances .- reflectances
 
-    fig, ax, plt = scatterlines(k₀s, 𝚃s, label="𝚃")
-    # scatterlines!(k₀s, 𝚁s, label="𝚁")
-    # scatterlines!(k₀s, E_lost, label=L"E_{lost}")
+    fig, ax, plt = scatterlines(k₀s, transmittances, label="Transmittance")
+    scatterlines!(k₀s, reflectances, label="Reflectance")
+    scatterlines!(k₀s, E_lost, label=L"E_{lost}")
     Legend(fig[1, 2], ax)
     ax.xlabel = "k₀"
     ax.ylabel = "Value"
-    fig #|> display
-    𝚃s|>display
+    fig |> display
+    # transmittances|>display
 end
-
 
 let # Verifying that abs2 of a complex number gives same as norm squared
     x = 17*cis(deg2rad(77))
