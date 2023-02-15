7d2ddf7a35810409fb379dd9f4993420a5440480
Enginedeer <andi.r.s@live.dk>
2023-02-15 16:16:41 +0100

    plotting analytical result

--- ComputationalPhysics/Homework/Andi_3.jl
+++ ComputationalPhysics/Homework/Andi_3.jl
@@ -17,49 +17,63 @@ display(eigen(A))
 
 ##
 
-N=400
-t₁=10
-t₀=0
-t=range(t₀,t₁,N)
-h=step(t)
-
-
-γ=0.5
-A=-1*[0 -1;
-      1 γ]
-x₀=[1;0]
-
-
 
 function euler(A,x₀,t)
     x=x₀
     h=step(t)
     xtot=zeros(size(x,1),size(t,1))
     xtot[:,1]=x
-    for i in 1:size(t,1)-1
+    for i in 2:size(t,1)
         dx=A*x
         x+=dx*h
-        xtot[:,i+1]=x
+        xtot[:,i]=x
     end
-
-    # Just plotting
-    Plots.plot(t,xtable[1,:], labels="Position")
-    Plots.plot!(t,xtable[2,:], labels="Velocity")
-    Plots.plot!(title = "Damped Harmonic Oscillator")
-    # Plots.plot!(legend=:topright)
-    Plots.xlabel!("t [s]")
-    harm_osc=Plots.ylabel!("Amplitude [m, m/s]")
-    display(harm_osc)
     
     return xtot
 end
 euler(args)=euler(args...)
 
+##
+
+N=5000
+t₁=100
+t₀=0
+t=range(t₀,t₁,N)
+h=step(t)
+
+
+γ=0.15
+A=-1*[0 -1;
+      1 γ]
+x₀=[1;0]
 
 xtable=euler(A,x₀,t)
 
+# Analytic solution
+if γ==2
+    γ+=2*eps()
+end
+
+α=x₀[1]
+β=x₀[2]
+Φ=√((γ/2)^2-1+0im)
+@show A=α/4*γ/Φ+α/2+β
+@show B=α-A
+
+s₁=-γ/2+√((γ/2)^2-1+0im)
+s₂=-γ/2-√((γ/2)^2-1+0im)
+anal=@.A*exp(s₁*t)+B*exp(s₂*t)
 
 
+# Just plotting. Should be changed for different use case
+plot(t,xtable[1,:], labels="Position")
+plot!(t,real.(anal),labels="Analytic position")
+plot!(t,xtable[2,:], labels="Velocity")
+plot!(title = "Damped Harmonic Oscillator")
+# Plots.plot!(legend=:topright)
+xlabel!("t [s]")
+harm_osc=ylabel!("Amplitude [m, m/s]")
+display(harm_osc)
 
 
 
