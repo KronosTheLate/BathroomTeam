f0091cb7d68f4817678fb843b7c0f19b6f8e8d93
KronosTheLate <densbal@live.no>
2023-02-21 15:57:10 +0100

    checkpoint as nice plotting for problem 2

--- AdvanvedPhysicalOptics/Dennis_Lect4_ImageFrequencyFiltering.jl
+++ AdvanvedPhysicalOptics/Dennis_Lect4_ImageFrequencyFiltering.jl
@@ -1,4 +1,46 @@
+using DelimitedFiles
 using Images
 using FFTW
+using GLMakie
 
-image_matrix = rand(512, 512)
+cd(joinpath(homedir(), "Uni", "Semester", "8. Sem", "Advanced Physical Optics", "BathroomTeamGithubFolder"))
+begin
+    # image_matrix = readdlm("lena.dat") |> x->reverse(x, dims=1)
+    image_matrix = Images.load("Vilde's pus, nå med farger!.png") .|> Gray .|> Float64
+    image_matrix = image_matrix ./ maximum(image_matrix)
+    image_matrix .|> Gray
+end
+
+##¤ Coupld of functions
+mynorm(A) = A ./ maximum(A)
+viz(A) = display(mynorm(A) .|> Gray)
+
+##¤ High pass filtering
+for n_freqs_pass = range(5, step=25, stop=200)
+    input_image = image_matrix
+    image_dft = fft(input_image)|>fftshift
+    zerofreq_inds = size(image_dft) .÷ 2
+
+    output_dft = ComplexF64[(abs(i - zerofreq_inds[1]) < n_freqs_pass && abs(j - zerofreq_inds[2]) < n_freqs_pass ? 0 : image_dft[i, j]) for i in axes(image_dft, 1), j in axes(image_dft, 2)]
+
+    show1 = input_image |> mynorm
+    show2 = image_dft .|>abs .|> log10 |> mynorm
+    show3 = output_dft .|> abs .|>log10 |> mynorm
+    show4 = ifft(output_dft|>fftshift) .|>abs |> mynorm
+    [show1 show2; show3 show4] |> viz
+end
+
+##¤ Low pass filtering
+for n_freqs_pass = range(5, step=25, stop=200) |> reverse
+    input_image = image_matrix
+    image_dft = fft(input_image)|>fftshift
+    zerofreq_inds = size(image_dft) .÷ 2
+
+    output_dft = ComplexF64[(abs(i - zerofreq_inds[1]) > n_freqs_pass || abs(j - zerofreq_inds[2]) > n_freqs_pass ? 0 : image_dft[i, j]) for i in axes(image_dft, 1), j in axes(image_dft, 2)]
+
+    show1 = input_image |> mynorm
+    show2 = image_dft .|>abs .|> log10 |> mynorm
+    show3 = output_dft .|> abs .|>log10 |> mynorm
+    show4 = ifft(output_dft|>fftshift) .|>abs |> mynorm
+    [show1 show2; show3 show4] |> viz
+end
--- ComputationalPhysics/Homework/Dennis_3.jl
+++ ComputationalPhysics/Homework/Dennis_3.jl
@@ -1,38 +1,53 @@
 using GLMakie; Makie.inline!(true)
-function integrate(state_to_rateofchange_matrix, t₀, t₁, steps, u⃗₀)
+using LinearAlgebra  # for `norm` of vectors
+#=
+function integrate_euler(state_to_rateofchange_matrix, t₀, t₁, steps, u⃗₀)
     timesteps = range(t₀, t₁, steps)
     h = step(timesteps)
     u⃗s = fill(69.0, length(u⃗₀), length(timesteps))  # For outputting
     u⃗s[:, 1] = u⃗₀  # init
     for i in 2:lastindex(timesteps)
-        Δu⃗ = state_to_rateofchange_matrix * u⃗s[:, i-1] * h
+        rate_of_change = state_to_rateofchange_matrix * u⃗s[:, i-1]
+        Δu⃗ = rate_of_change * h
         u⃗s[:, i] = u⃗s[:, i-1] + Δu⃗
     end
-    return (us = u⃗s, timesteps=timesteps, h=h)
+    return (us = u⃗s, timesteps=timesteps)
 end
-integrate(args) = integrate(args...)
+integrate_euler(args) = integrate_euler(args...)
+=#
 
-##
-t0 = 0
-t1 = 100
-# desired_hs = (1, 0.1, 0.01, 0.001)
-
-N = 5000
-γ = 0.15
-if γ == 2
-    γ += 2eps()  # Workaround
+function integrate_euler(∇::Function, t₀, t₁, steps, u⃗₀)
+    timesteps = range(t₀, t₁, steps)
+    h = step(timesteps)
+    u⃗s = Vector{typeof(u⃗₀)}(undef, length(timesteps))  #fill(69.0, length(u⃗₀), length(timesteps))  # For outputting
+    u⃗s[1] = u⃗₀  # init
+    for i in 2:lastindex(timesteps)
+        rate_of_change = ∇(u⃗s[i-1])
+        Δu⃗ = rate_of_change * h
+        u⃗s[i] = u⃗s[i-1] .+ Δu⃗
+    end
+    return (us = u⃗s, timesteps=timesteps)
 end
-A = [0 1; -1 -γ]
+integrate_euler(args) = integrate_euler(args...)
 
-prob1 = (A, t0, t1, N, [1, 0])
 
-# function euler(prob)
-let prob = prob1
-    us, timesteps, h = integrate(prob)
-    step_ = step(timesteps)
-    # us|>display
-    positions = us[1, :]
-    velocities = us[2, :]
+let #? Task 1, plotting single case
+    begin
+        t0 = 0
+        t1 = 10
+        N = 500
+        γ = 0.15
+        if γ == 2
+            γ += 2eps()  # Workaround
+        end
+        A(u⃗) = [0.0 1; -1 -γ] * u⃗
+        prob = (A, t0, t1, N, [1.0, 0])
+    end
+
+    us, timesteps = integrate_euler(prob)
+    h = step(timesteps)
+    positions = getindex.(us, 1)
+    velocities = getindex.(us, 2)
     fig, ax, plt = lines(timesteps, positions, label="Positions")
     α = prob[end][1]
     β = prob[end][2]
@@ -53,23 +68,24 @@ let prob = prob1
     # ax.xscale=log10
     fig |> display
 end
-# euler(prob1)
 
-function task_2d(γ, timestep)
+##¤ Task 1d
+function task_1d(γ, timestep)
     t0 = 0
     t1 = 100
     
     N = round(Int64, (t1-t0)/timestep)
-    println("Desired step: $timestep")
-    println("True step   : $(step(range(0, 100, N)))")
+    # println("Desired step: $timestep")
+    # println("True step   : $(step(range(0, 100, N)))")
     if γ == 2
         γ += 2eps()  # Workaround
     end
-    A = [0 1; -1 -γ]
+    A(u⃗) = [0.0 1; -1 -γ] * u⃗
 
-    prob = (A, t0, t1, N, [1, 0])
-    us, timesteps, h = integrate(prob)
-    positions = us[1, :]
+    prob = (A, t0, t1, N, [1.0, 0])
+    us, timesteps = integrate_euler(prob)
+    h = step(timesteps)
+    positions = getindex.(us, 1)
     α = prob[end][1]
     β = prob[end][2]
     Φ = √(γ^2/4-1+0im)
@@ -84,12 +100,109 @@ function task_2d(γ, timestep)
     return (timesteps = timesteps, pos_est=positions, pos_ana=real.(us_analytical), resids=r⃗, errors=errors)
 end
 
-let
+with_theme(markersize=5, resolution=(1920÷1.75, 1080÷1.5)) do
+    for γ in (0.5, 1, 2, 4) # Un, under, critical, over
+        fig = Figure()
+        for (i, timestep) in enumerate([1, 1e-1, 1e-2, 1e-3][1:3])
+            timesteps, pos_est, pos_ana, resids, errors = task_1d(γ, timestep)
+            let #! Linear-scale plot
+                ax = Axis(fig[1, i], title="timestep = $timestep")
+                scatterlines!(timesteps, pos_est, label="Estimated position")
+                scatterlines!(timesteps, resids,  label="Residuals")
+                scatterlines!(timesteps, pos_ana, label="Analytical position")
+                scatterlines!(timesteps, errors,  label="Error up to timestep")
+                Legend(fig[4, 2:3], ax, nbanks=2)
+                Label(fig[1, 0], "Lin x & y", rotation=π/2, tellheight=false)
+            end
+            let #! Log-scale plots
+                thresh_y = 1e-20
+                thresh_timesteps = 1e-1
+                increase_floor(v::AbstractVector, thresh=thresh_y) = replace(x->abs(x) ≥ thresh ? x : thresh, v) 
+                logproc(x) = x .|> abs |> increase_floor
+                timesteps = increase_floor(timesteps, thresh_timesteps)
+                ax = Axis(fig[2, i])
+
+                scatterlines!(timesteps, logproc(pos_est))
+                scatterlines!(timesteps, logproc(resids) )
+                scatterlines!(timesteps, logproc(pos_ana))
+                scatterlines!(timesteps, logproc(errors) )
+                # ax.xscale = log10
+                ax.yscale = log10
+                Label(fig[2, 0], "Lin x, Log y", rotation=π/2, tellheight=false)
+
+                ax = Axis(fig[3, i])
+                scatterlines!(timesteps, logproc(pos_est))
+                scatterlines!(timesteps, logproc(resids) )
+                scatterlines!(timesteps, logproc(pos_ana))
+                scatterlines!(timesteps, logproc(errors) )
+                ax.yscale = log10
+                ax.xscale = log10
+                Label(fig[3, 0], "Log x & y", rotation=π/2, tellheight=false)
+            end
+        end
+        Label(fig[4, 1], "γ = $γ", tellwidth=false, textsize=25)
+        fig |> display
+    end
+end
+
+##¤ Task 2
+# state_to_rateofchange_matrix goes from const to function of state
+# The function is renamed to `∇`, and is applied to the current state
+
+with_theme(resolution = (1920÷2, 1080÷2) ,markersize=5) do
+    #? Problem definition
+    ∇(u⃗) = [0 1; -1/norm(u⃗)^3 0]*u⃗
+    t0 = 0
+    t1 = 50
+    N = 2000
+    p⃗₀ = 0.5
+    prob = (∇, t0, t1, N, [[1.0, 0.0], [0, p⃗₀]])
+
+    us, timesteps = integrate_euler(prob)
+    h = step(timesteps)
+    rs = getindex.(us, 1)
+    r_xs = getindex.(rs, 1)
+    r_ys = getindex.(rs, 2)
+
+    ps = getindex.(us, 2)
+    p_xs = getindex.(ps, 1)
+    p_ys = getindex.(ps, 2)
+
+    #? Plotting
+    begin  # Plotting components
+        fig = Figure()
+        ax1 = Axis(fig[1, 1], xlabel="Timestep", ylabel="X component")
+        scatterlines!(timesteps, r_xs, label=L"\vec{r}_x", color = Cycled(1)) # , marker='→'
+        scatterlines!(timesteps, p_xs, label=L"\vec{p}_x", color = Cycled(2)) # , marker='→'
+        ax2 = Axis(fig[2, 1], ylabel="Y component")
+        scatterlines!(timesteps, r_ys, label=L"\vec{r}_y", color = Cycled(1)) # , marker='↑'
+        scatterlines!(timesteps, p_ys, label=L"\vec{p}_y", color = Cycled(2)) # , marker='↑'
+        linkxaxes!(ax1, ax2)
+        # Legend(fig[2, 1], ax, tellwidth=false, tellheight=true, nbanks=2)
+    end
+    begin  # Plotting orbits
+        # fig = Figure()
+        ax = Axis(fig[1:2, 3], title="Orbit", ylabel="Y coordinate", xlabel="X coordinate", yaxisposition=:right)
+        scatterlines!(r_xs, r_ys, label=L"\vec{r}", color = Cycled(1), markersize=timesteps ./ t1 .* 10)
+        scatterlines!(p_xs, p_ys, label=L"\vec{p}", color = Cycled(2), markersize=timesteps ./ t1 .* 10)
+        Legend(fig[:, 2], ax)#, tellwidth=false, tellheight=true)
+    end
+    # r⃗ = positions .- real.(us_analytical) # Residuals
+    # errors = [√sum(r⃗[begin:i].^2 .* h) for i in eachindex(r⃗)]
+    
+    
+    fig |> display
+end
+
+
+
+#=
+with_theme(markersize=5) do
 counter = 0
 for γ in (0.5, 1, 2, 4) # Un, under, critical, over
-    for timestep in (1, 1e-1, 1e-2, 1e-3)[1:2]
+    for timestep in (1, 1e-1, 1e-2, 1e-3)
         counter += 1
-        timesteps, pos_est, pos_ana, resids, errors = task_2d(γ, timestep)
+        timesteps, pos_est, pos_ana, resids, errors = task_1d(γ, timestep)
         let #! Linear-scale plots
             fig, ax, plt = lines(timesteps, pos_est, label="Estimated position")
             ax.title = "γ = $γ, h = $timestep"
@@ -105,19 +218,23 @@ for γ in (0.5, 1, 2, 4) # Un, under, critical, over
             increase_floor(v::AbstractVector, thresh=thresh_y) = replace(x->abs(x) ≥ thresh ? x : thresh, v) 
             thresh_timesteps = 1e-1
             timesteps = increase_floor(timesteps, thresh_timesteps)
-            fig, ax, plt = lines(timesteps, abs.(pos_est) |> increase_floor, label="Estimated position")
-            ax.title = "γ = $γ, h = $timestep\nthresh_y = $thresh_y, thresh_x = $thresh_timesteps"
-            lines!(timesteps, increase_floor(abs.(pos_ana)) , label="Analytical position")
-            lines!(timesteps, increase_floor(abs.(resids))  , label="Residuals")
-            lines!(timesteps, increase_floor(abs.(errors))  , label="Error up to timestep")
+            fig = Figure()
+            ax = Axis(fig[1, 1])
+            mytitle = "γ = $γ, h = $timestep"
+            # mytitle *= "\nthresh_y = $thresh_y, thresh_x = $thresh_timesteps"
+            ax.title = mytitle
+            scatterlines!(timesteps, abs.(pos_est) |> increase_floor, label="Estimated position")
+            scatterlines!(timesteps, increase_floor(abs.(pos_ana)) , label="Analytical position")
+            scatterlines!(timesteps, increase_floor(abs.(resids))  , label="Residuals")
+            scatterlines!(timesteps, increase_floor(abs.(errors))  , label="Error up to timestep")
             Legend(fig[1, 2], ax)
             ax.yscale = log10
             fig |> display
             ax.xscale = log10
             fig |> display
         end
-        @info "Finnished with $counter/16 plots"
+        @info "Finnished with plot $counter"
     end
 end
 end
-
+=#
