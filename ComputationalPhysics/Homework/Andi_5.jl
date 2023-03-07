eca194da0e8740a02d5ee25a485d86b2d35d324c
Enginedeer <andi.r.s@live.dk>
2023-03-07 22:50:23 +0100

    Done with problem Set 5. But rough on the edges

--- ComputationalPhysics/Homework/Andi_5.jl
+++ ComputationalPhysics/Homework/Andi_5.jl
@@ -2,7 +2,10 @@ using LinearAlgebra
 using Random # random number in "rand"
 using Distributions # uniform distribution with "Uniform()"
 using Plots
-plotly() # Interactive plots
+# plotly() # Interactive plots
+gr()
+# using PyPlot
+
 
 # Only fitted to Keplar problem, since knowledge of underlying variables seems needed.
 function leap_frog(f,x₀,t,R)
@@ -11,6 +14,7 @@ function leap_frog(f,x₀,t,R)
     xtot=zeros(size(x,1),size(x,2),size(t,1))
     xtot[:,:,1]=x
     for i in 2:size(t,1)
+        if t[i]>25 R=[1;0;0] end #Quick and dirty task 1b for particle box moving
         dx=f(x,R)
         # display(x)
         # display(dx)
@@ -60,6 +64,25 @@ function d_dt_kepler(states,R)
         dpz[i]=dp[3]
     end
 
+    if true # Workaround to implement repulsive forces
+        #Implementation of repulsive force between particles. Can be toggled on and off
+        F=zeros(N,3)
+        α=1/N
+        for j in 1:N #Note could cut computation time down, since every computation is calculated twice. 
+                     #E.g. j=2 and jj=3 is similar but with *-1 as j=3 and jj=2
+            for jj in 1:N
+                if jj != j
+                    rj=[rx[j];ry[j];rz[j]]
+                    rjj=[rx[jj];ry[jj];rz[jj]]
+                    F[j,:]+=α*(rj-rjj) / norm(rj-rjj)^3
+                end
+            end
+            dpx[j]+=F[j,1]
+            dpy[j]+=F[j,2]
+            dpz[j]+=F[j,3]
+        end
+    end
+
     return hcat(drx, dry, drz, dpx, dpy, dpz)
 end
 
@@ -68,13 +91,13 @@ end
 
 
 t₀=0
-t₁=1
-Δt=0.3
+t₁=50
+Δt=0.01
 Nt=Int(round((t₁-t₀)/Δt))
 t=range(t₀,t₁,Nt)
 
 
-N=3
+N=100
 
 
 
@@ -96,59 +119,34 @@ R=[0; 0; 0]
 
 states_total=leap_frog(d_dt_kepler,states₀,t,R)
 
-# x=copy(states₀)
-# h=step(t)
-# xtot=zeros(size(x,1),size(x,2),size(t,1))
-# xtot[:,:,1]=x
-# # for i in 2:size(t,1)
-#     display(x)
-#     dx=f(x)
-#     x[:,1:3]+=dx[:,1:3]*h # Could interchange position and momentum update. 1:2->3:4
-#     display(x)
-#     dx=f(x)
-#     x[:,4:6]+=dx[:,4:6]*h # Could interchange position and momentum update. 3:4->1:2
-#     xtot[:,:,3]=x
-# # end
-
-display(states_total)
 
 rx=states_total[:,1,:]
 ry=states_total[:,2,:]
 rz=states_total[:,3,:]
-Plots.scatter(rx[:,1],ry[:,1],rz[:,1])
-i=1
-plot(rx[:,i],ry[:,i],rz[:,i], vars = (1,2,3), tspan = (0.0, t), lab = "Solution 1")
 
 ##
 
-nframes = Nt # Change later for large Nt
-anim = @animate for (i,t) in enumerate(LinRange(first(t), last(t), nframes))
-    plot(rx[:,i],ry[:,i],rz[:,i], vars = (1,2,3), tspan = (0.0, t), lab = "Solution 1")
-    plot!(sol2, vars = (1,2,3), tspan = (0.0, t), lab = "Solution 2")
+t_range=range(t₀,t₁,Nt)
+#https://discourse.julialang.org/t/3d-animation-plot-of-2-ode-solutions/83408
+nframes = 200 # Change later for large Nt
+# anim = @animate for t in LinRange(first(t), last(t), nframes)
+anim = @animate for j in LinRange(1, size(rx,2), nframes)
+    i=Int(round(j))
+    scatter(rx[:,i],ry[:,i],rz[:,i], legend=false)#, color=3, lab = "time=$(round(t, digits = 2))")
+    # plot!(sol2, vars = (1,2,3), tspan = (0.0, t), lab = "Solution 2")
     
     #current time marker
-    scatter!(sol1, vars = (1,2,3), tspan = (t, t), lab = nothing, color = 1)
-    scatter!(sol2, vars = (1,2,3), tspan = (t, t), lab = nothing, color = 2)
+    # scatter!(rx[:,i],ry[:,i],rz[:,i], vars = (1,2,3), tspan = (t, t), lab = nothing, color = 1)
+    # scatter!(sol2, vars = (1,2,3), tspan = (t, t), lab = nothing, color = 2)
     
     # axis setting... need to be last b/c DiffEq recipes will overwrite.
-    plot!(xaxis = ("x" ,(-30, 30)), yaxis = ("y", (-30,30)), zaxis=("z", (0, 60)),
-        title = "t = $(round(t, digits = 2))")
+    # plot!(xaxis = ("x" ,(-30, 30)), yaxis = ("y", (-30,30)), zaxis=("z", (0, 60)),
+        # title = "t = $(round(t, digits = 2))")
+    view=2
+    plot!(xaxis = ("x" ,(-view, view)), yaxis = ("y", (-view,view)), zaxis=("z", (-view, view)),title = "N=$N, t = $(round(t[i], digits = 2))")
 end
 
-gif(anim, "myGif.gif"; fps = 30)
+gif(anim, "C:\\Users\\andir\\Downloads\\Andi_5_subtask_a.gif"; fps = 20)
 
 
 
-##
-
-# states=[1 1 1 1 1 1 ; 2 2 2 2 2 2]
-# change=d_dt_kepler(states,[0;0;0])
-# display(change)
-
-# cat(reshape(states,2,6,1),reshape(change,2,6,1),dims=3)
-
-
-##
-
-rng = MersenneTwister(1234)
-rand(rng, Uniform(-0.5,0.5),N)
