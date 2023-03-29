df4496d7d273cb6047926c1787fc9b915cf618fd
KronosTheLate <densbal@live.no>
2023-03-29 09:55:31 +0200

    sync

--- ComputationalPhysics/Homework/Dennis_8.jl
+++ ComputationalPhysics/Homework/Dennis_8.jl
@@ -44,12 +44,14 @@ function parameters(N::Integer; tol=2)  # Function, because want to update for c
     # κ = 0.25
     return (;tol, κ, ω, xs, k₀, tspan, dt)
 end
-p = parameters(200)
+p = parameters(50)
+
 #¤ "Implement function that represent Hamiltonian"
 function Hψ(ψ, p)
     (;k₀, tol, xs) = p      # NamedTuple descructuring
     ∂ₓ²ψ = approximate_2nd_derivative(ψ, k₀; tol)
     return @. 1/2 * (-∂ₓ²ψ + xs^2 * ψ)
+    # return @. 1/2 * (-∂ₓ²ψ + xs^4 * ψ)
 end
 ##¤ du: slope vector (will be updated)
 #¤  u: current state
@@ -74,9 +76,9 @@ sol = solve(prob, RK4(); adaptive=false, p.dt)
 
 post_processor(solution) = reduce(hcat, solution) .|> abs2
 log10_offset(x) = log10(x+1e-7)
-scale = identity
-scale = log10
-scale = log10_offset
+# scale = identity
+# scale = log10
+# scale = log10_offset
 
 u_ref = [u_ref_func(t, p.xs) for t in sol.t]
 u_resid = sol.u - u_ref 
@@ -85,8 +87,13 @@ sol_u_processed = sol.u |> post_processor .|> scale
 u_ref_processed = u_ref |> post_processor .|> scale
 u_resid_processed = u_resid |> post_processor .|> scale
 
-energy_expectationval = [ψ' * Hψ(ψ, p) * step(p.xs) for ψ in sol.u]
-
+begin
+    energy_expectationval = [ψ' * Hψ(ψ, p) * step(p.xs) for ψ in sol.u]
+    lines(sol.t, energy_expectationval.|>real, label="Real part", axis=(xlabel="Time", ylabel="Energy"))
+    lines!(sol.t, energy_expectationval.|>imag, label="Imag part")
+    axislegend(position=(0, 0.5))
+    current_figure()|>display
+end
 function get_lims(data...)  # Useful for setting common colorbars
     extremas = extrema.(data)
     lowest = minimum(getindex.(extremas, 1))
@@ -118,7 +125,7 @@ end
 
 ##¤ Convergence study
 
-function L∞∞_func(N)
+function L∞∞(N)
     p = parameters(N; tol=100)
     (;xs, tspan, dt) = p
     u0 = u_ref_func(0, xs)
@@ -130,7 +137,7 @@ function L∞∞_func(N)
     return norm(norm.(u_resid, Inf), Inf)
 end
 
-function L∫∫_func(N)
+function L∫∫(N)
     p = parameters(N; tol=100)
     (;xs, tspan, dt) = p
     u0 = u_ref_func(0, xs)
@@ -143,13 +150,13 @@ function L∫∫_func(N)
     integrated_over_space = [h * sum(abs.(residuals)) for residuals in u_resid]
     integrated_over_time = dt * sum(abs.(residuals) for residuals in integrated_over_space)
     return abs(integrated_over_time)
-    
 end
 ##
-Ns = [round(Int, 100*1.2^i) for i in 0:10]
+# Ns = [round(Int, 100*1.2^i) for i in 0:5]
+Ns = 20:2:150
 dts = getproperty.(parameters.(Ns), :dt)
-errorfunc = L∞∞_func
-# errorfunc = L∫∫_func
+errorfunc = L∞∞
+# errorfunc = L∫∫
 
 # scale = identity
 # scale = log10
@@ -158,8 +165,8 @@ errorfunc = L∞∞_func
 errors = errorfunc.(Ns)
 mask_convergence = isfinite.(errors)
 let
-    fig, ax, plt = lines(dts[mask_convergence], errors[mask_convergence])
-    ax.xlabel = "Temporal stepsize"
+    fig, ax, plt = lines(Ns[mask_convergence], errors[mask_convergence])
+    ax.xlabel = "N"
     ax.ylabel = "Error measure"
     ax.yscale = log10
     ax.xscale = log10
@@ -171,16 +178,20 @@ end
 
 ##¤ d) Different initial conditions
 
+p = parameters(100)
+length(p.xs)
 # u0 = u_ref_func(0, p.xs)
 # u0 = ComplexF64[exp(-0.25x^2) for x in p.xs]
-u0 = ComplexF64[exp(-0.25(x-4)^2) for x in p.xs]
-# u0 = ComplexF64[exp(-0.5(x-4)^2) for x in p.xs]
-# u0 = ComplexF64[exp(  -2(x-4)^2) for x in p.xs]
+# u0 = ComplexF64[exp(-0.25(x-4)^2) for x in p.xs]
+# u0 = ComplexF64[exp(-0.5(x-4)^2) for x in p.xs]   #¤ Not even changing shape, just oscillatin
+# u0 = ComplexF64[exp(-2x^2) for x in p.xs]         #¤ "Breathing"/"pumping"
+# u0 = ComplexF64[exp(  -2(x-4)^2) for x in p.xs]   #¤ "Pumping" + oscillation
 # u0 = ComplexF64[exp(  -2(x-4)^2) - exp(  -2(x+4)^2) for x in p.xs]
+u0 = ComplexF64[exp(  -2(x-4)^2) + exp(  -2(x+4)^2) for x in p.xs]
 prob = ODEProblem(update_slope_schr!, u0, p.tspan, p)
 sol = solve(prob, RK4(); adaptive=false, p.dt)
 
-scale = log10
+# scale = log10
 scale = log10_offset
 sol_u_processed = sol.u |> post_processor .|> scale
 let  #¤ Plotting
@@ -188,7 +199,9 @@ let  #¤ Plotting
     ax.xlabel="x"
     ax.ylabel="time"
     ax.title = "Numerical solution"
+    ax2, hm2 = heatmap(fig[2, 1], p.xs, sol.t, angle.(reduce(hcat, sol.u)))
+    Label(fig[0, :], string("Scale = ", scale), tellwidth=false)
     Colorbar(fig[1, 2], hm)
-    Label(fig[0, :], string("Scale = ", scale))
+    Colorbar(fig[2, 2], hm2)
     display(fig)
 end
